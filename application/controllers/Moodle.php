6ba4b8c9c3f9342af2022ae4b8a6763b7849f135
joash <njovujsh@gmail.com>
2020-10-16 12:29:12 +0300

    login now

--- application/controllers/Moodle.php
+++ application/controllers/Moodle.php
@@ -16,7 +16,7 @@ class Moodle extends CI_Controller
     {
         // echo $this->hash_internal_user_password("Thijs123!@#");
     }
-    public function login($var = null)
+    public function login_old_v1($var = null)
     {
         // $_POST['email'] = "megasega91@gmail.com";
         // $_POST['password'] = "Mega1java123!@#";
@@ -50,5 +50,26 @@ class Moodle extends CI_Controller
 
         $server_output = curl_request('http://localhost/heapp/login', array('email' => 'megasega91@gmail.com', 'password' => 'Mega1java123!@#'), "post", array('App-Key: 123456'));
         print_array($server_output);
+    }
+    public function login($var = null)
+    {
+        $_POST['username'] = "testuser_testuser";
+        $_POST['password'] = "Newuser123!";
+        if (isset($_POST['username']) && isset($_POST['password'])) {
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            $domainname = 'https://app.healthyentrepreneurs.nl';
+            $serverurl = $domainname . '/login/token.php';
+            $data = array(
+                'username' => $username,
+                'password' => $password,
+                'service' => 'moodle_mobile_app'
+            );
+            $server_output = curl_request($serverurl, $data, "get", array('App-Key: 123456'));
+            $array_of_output = json_decode($server_output, true);
+            echo empty_response("New User Successfully Created", 200, $array_of_output);
+        } else {
+            echo empty_response("Credentials Are Required");
+        }
     }
 }
