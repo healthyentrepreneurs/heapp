26c2e378bde21b80aee3932829337244f815cdac
joash <njovujsh@gmail.com>
2020-12-20 21:54:10 +0300

    books phase 1

--- application/controllers/Downloadable.php
+++ application/controllers/Downloadable.php
@@ -94,35 +94,42 @@ class Downloadable extends CI_Controller
                     fclose($file);
                     $value_course['next_link'] = '/next_link/survey/' . $value_course['id'] . ".json";
                     #Handle Images For Survey
-                    // print_array($value_course['image_url_small']);
-                    // print_array($value_course['image_url']);
-                    $_image_small_arr = explode('/', $value_course['image_url_small']);
-                    $_image_big_arr = explode('/', $value_course['image_url']);
-                    $imgn_x = $user_id .  $_image_small_arr[count($_image_small_arr) - 1];
-                    $img_twon_x = $user_id . $_image_big_arr[count($_image_big_arr) - 1];
-                    $img_n = $img_survey . '/' . $imgn_x;
-                    $img_two_n = $img_survey . '/' . $img_twon_x;
-                    $file_headers = @get_headers($value_course['image_url_small']);
-                    if (!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
-                        // print_array("No image_url_small");
-                        file_put_contents($img_n, file_get_contents(base_url('uploadicons/60_user_profile_pic39K.png')));
-                    } else {
-                        file_put_contents($img_n, file_get_contents($value_course['image_url_small']));
-                    }
-                    $file_headers_n = @get_headers($value_course['image_url']);
-                    if (!$file_headers_n || $file_headers_n[0] == 'HTTP/1.1 404 Not Found') {
-                        // print_array("No image_url");
-                        file_put_contents($img_two_n, file_get_contents(base_url('uploadicons/600_user_profile_pic39K.png')));
-                    } else {
-                        file_put_contents($img_two_n, file_get_contents($value_course['image_url']));
-                    }
+                    $image_path = $this->getme_images($img_survey, $user_id, $value_course);
                     // file_put_contents($img_two_n, file_get_contents($value_course['image_url']));
-                    $value_course['image_url_small'] = '/images/survey/' . $imgn;
-                    $value_course['image_url'] = '/images/survey/' . $img_twon;
+                    $value_course['image_url_small'] = '/images/survey/' . $image_path['image_url_small'];
+                    $value_course['image_url'] = '/images/survey/' . $image_path['image_url'];
+                } elseif ($value_course['source'] == "moodle") {
+                    print_array($value_course);
+                    $course_nextlink = $value_course['next_link'];
+                    $course_nextlink_array = explode('/', $course_nextlink);
+                    $dir_get_details_percourse = $dir_nextlink . "get_details_percourse";
+                    if (!is_dir($dir_get_details_percourse)) {
+                        mkdir($dir_get_details_percourse, 0755, true);
+                    }
+                    $dir_course_id = $dir_get_details_percourse . DIRECTORY_SEPARATOR . $course_nextlink_array[count($course_nextlink_array) - 2];
+                    if (!is_dir($dir_course_id)) {
+                        mkdir($dir_course_id, 0755, true);
+                    }
+                    //Post Book Phase 1
+                    $server_output_book = curl_request($value_course['next_link'], $data_course, "post", array('App-Key: 123456'));
+                    $file_n = fopen($dir_course_id . '/' . $course_nextlink_array[count($course_nextlink_array) - 2] . ".json", "w");
+                    fwrite($file_n, $server_output_book);
+                    fclose($file_n);
+                    //Change Nextlink Books
+                    $value_course['next_link'] = '/' . $course_nextlink_array[count($course_nextlink_array) - 2] . ".json";
+                    $value_course['next_link'] =  '/next_link/get_details_percourse/' . $course_nextlink_array[count($course_nextlink_array) - 2] . '/' . $course_nextlink_array[count($course_nextlink_array) - 2] . ".json";
+                    $img_course = $mypath . 'images' . DIRECTORY_SEPARATOR . 'course';
+                    if (!is_dir($img_course)) {
+                        mkdir($img_course, 0755, true);
+                    }
+                    $img_course_id = $img_course . DIRECTORY_SEPARATOR . $course_nextlink_array[count($course_nextlink_array) - 2];
+                    if (!is_dir($img_course_id)) {
+                        mkdir($img_course_id, 0755, true);
+                    }
+                    $image_pathn = $this->getme_images($img_course_id, $user_id, $value_course);
+                    $value_course['image_url_small'] = '/images/course/' . $course_nextlink_array[count($course_nextlink_array) - 2] . '/' . $image_pathn['image_url_small'];
+                    $value_course['image_url'] = '/images/course/' . $course_nextlink_array[count($course_nextlink_array) - 2] . '/' . $image_pathn['image_url'];
                 }
-                // elseif($value_course['source'] == "originalm"){
-
-                // }
                 array_push($modifyied_courses, $value_course);
             }
             $modifyied_courses_json = json_encode($modifyied_courses);
@@ -154,7 +161,6 @@ class Downloadable extends CI_Controller
             }
             // Zip archive will be created only after closing object
             $zip->close();
-            // echo empty_response("Zip File Successfully Downloaded", 200);
             header('Content-disposition: attachment; filename=HE Health.zip');
             header('Content-type: application/zip');
             readfile($tmp_file);
@@ -162,8 +168,48 @@ class Downloadable extends CI_Controller
     }
     public function downloadBook()
     {
-        
+
         // $value_course['image_url_small'] = '/images/survey/' . $imgn;
         // $value_course['image_url'] = '/images/survey/' . $img_twon;
+    }
+    public function getme_images($img_survey, $user_id, $value_course)
+    {
+        //Duplicate Images for download
+        $image_url_smalloriginal = $value_course['image_url_small'];
+        $image_url_original = $value_course['image_url'];
+        //End
+        if (strpos($value_course['image_url_small'], '?')) {
+            $image_url_small_array = explode('?', $value_course['image_url_small']);
+            $value_course['image_url_small'] = $image_url_small_array[0];
+        }
+        if (strpos($value_course['image_url'], '?')) {
+            $image_url_array = explode('?', $value_course['image_url']);
+            $value_course['image_url'] = $image_url_array[0];
+        }
+        $_image_small_arr = explode('/', $value_course['image_url_small']);
+        $_image_big_arr = explode('/', $value_course['image_url']);
+
+        $imgn_x = $user_id .  $_image_small_arr[count($_image_small_arr) - 1];
+        $img_twon_x = $user_id . $_image_big_arr[count($_image_big_arr) - 1];
+        $img_n = $img_survey . '/' . $imgn_x;
+        $img_two_n = $img_survey . '/' . $img_twon_x;
+        $file_headers = @get_headers($image_url_smalloriginal);
+        if (!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
+            // print_array("No image_url_small");
+            file_put_contents($img_n, file_get_contents(base_url('uploadicons/60_user_profile_pic39K.png')));
+        } else {
+            file_put_contents($img_n, file_get_contents($image_url_smalloriginal));
+        }
+        $file_headers_n = @get_headers($image_url_original);
+        if (!$file_headers_n || $file_headers_n[0] == 'HTTP/1.1 404 Not Found') {
+            // print_array("No image_url");
+            file_put_contents($img_two_n, file_get_contents(base_url('uploadicons/600_user_profile_pic39K.png')));
+        } else {
+            file_put_contents($img_two_n, file_get_contents($image_url_original));
+        }
+        return array(
+            'image_url_small' => $imgn_x,
+            'image_url' => $img_twon_x
+        );
     }
 }
