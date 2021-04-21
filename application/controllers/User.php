ed74147730915e4f8524b25918c894ad787cbe24
njovu <njovujsh@gmail.com>
2021-04-21 13:37:43 +0000

    report two n

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -368,7 +368,7 @@ class User extends CI_Controller
         return $array_object;
     }
     #Test Get User Details
-    public function viwedbook($book_id, $chapter_id,$token,$username=0,$course_id=0)
+    public function viwedbook($book_id, $chapter_id, $token, $username = 0, $course_id = 0)
     {
         // http://localhost/m/stable_master/webservice/rest/server.php?moodlewsrestformat=json' --data 'bookid=1&chapterid=1&wsfunction=mod_book_view_book&wstoken=a70d553bbaf6d9b260a9e5c701b3c46e
         $domainname = 'https://app.healthyentrepreneurs.nl';
@@ -384,7 +384,7 @@ class User extends CI_Controller
         );
         $server_output = curl_request($serverurl, $data, "get", array('App-Key: 123456'));
         // return $server_output;
-        $this->universal_model->insertzwhere('viewtable', array('book_id' => $book_id, 'view_id' => $chapter_id, 'token' => $token,'user_id'=>$username,'course_id'=>$course_id));
+        $this->universal_model->insertzwhere('viewtable', array('book_id' => $book_id, 'view_id' => $chapter_id, 'token' => $token, 'user_id' => $username, 'course_id' => $course_id));
         // public function insertzwhere($table_name, $array_value)
         $array_of_output = json_decode($server_output, true);
         if (!empty($array_of_output)) {
@@ -410,6 +410,36 @@ class User extends CI_Controller
         // );
         // $server_output = curl_request($serverurl, $data, "get", array('App-Key: 123456'));
         // print_array($server_output);
+    }
+    public function test_extraction($_courseid, $token, $book_id, $chapter_id)
+    {
+        //   public function get_details_percourse($_courseid, $token, $show = 1)
+        $data_analysis = $this->get_details_percourse($_courseid, $token);
+        $chaptername = "";
+        $modicon = "";
+        $contents = "";
+        $stop_search = false;
+        foreach ($data_analysis as $value_books) {
+            $modules = $value_books['modules'];
+            foreach ($modules as  $module) {
+                if ($module['instance'] == $book_id) {
+                    $chaptername = $module['name'];
+                    $modicon = $module['modicon'];
+                    $contents = $module['contents'][0];
+                    $stop_search = true;
+                    break;
+                }
+            }
+            if ($stop_search) {
+                break;
+            }
+        }
+        $array_data = array(
+            $chaptername,
+            $modicon,
+            $contents
+        );
+        print_array($array_data);
     }
     public function get_admin_token()
     {
