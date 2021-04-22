15b1499773fa085f8c6c322d1d46f5ce495e59a9
njovu <njovujsh@gmail.com>
2021-04-22 02:24:22 +0000

    added test books report

--- application/views/pages/table/chapterchapter_table.php
+++ application/views/pages/table/chapterchapter_table.php
@@ -2,7 +2,7 @@
     <thead>
         <tr>
             <th class="center">FULL NAMES</th>
-            <th class="center">SHORT NAME</th>
+            <th class="center">COURSE</th>
             <th class="center">BOOK NAME</th>
             <th class="center"></th>
             <th class="center">CHAPTER NAME</th>
@@ -16,30 +16,12 @@
         ?>
             <tr>
                 <td class="center"><?= $value_in_sub['he_names']  ?></td>
-                <td class="center"><?='<b>' . $value_in_sub['course_shortname'] . '<b>'   ?></td>
+                <td class="center"><?= '<b>' . $value_in_sub['course_shortname'] . '<b>'   ?></td>
                 <td class="center"><?= $value_in_sub['book_name'] ?></td>
                 <td class="center"><img src="<?= $value_in_sub['modicon_chapter'] ?>" alt="image" width="100" height="100" /></td>
+                <td class="center"><?= $value_in_sub['chaptername'] ?></td>
                 <td class="center"><?= date("F jS, Y", strtotime($value_in_sub['date_inserted'])) ?></td>
                 <td class="center"><?= date("h:i:sa", strtotime($value_in_sub['date_inserted'])) ?></td>
-                <!-- <td class="center">
-                    <div class="visible-md visible-lg hidden-sm hidden-xs">
-                        <a href="<?= base_url('welcome/admin/11/' . $value_in_sub['id']) ?>" class="btn btn-xs btn-green tooltips" data-placement="top" data-original-title="More Details" target="_blank"><i class="fa fa-share"></i></a>
-                    </div>
-                    <div class="visible-xs visible-sm hidden-md hidden-lg">
-                        <div class="btn-group">
-                            <a class="btn btn-green dropdown-toggle btn-sm" data-toggle="dropdown" href="#">
-                                <i class="fa fa-cog"></i> <span class="caret"></span>
-                            </a>
-                            <ul role="menu" class="dropdown-menu pull-right dropdown-dark">
-                                <li>
-                                    <a role="menuitem" tabindex="-1" href="">
-                                        <i class="fa fa-share"></i> Book Details
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </td> -->
             </tr>
         <?php
         }
