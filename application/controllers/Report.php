6e50d4b54db6e4570e5531c61580b0097f136a49
njovu <njovujsh@gmail.com>
2021-04-22 01:34:20 +0000

    added test books report

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -624,16 +624,16 @@ class Report extends CI_Controller
         $courseid = 1;
         $bookid = 1;
         echo json_encode($_POST);
-        // if (!array_key_exists('courseid', $_POST)) {
-        //     $courseid = 0;
-        // }
-        // if (!array_key_exists('bookid', $_POST)) {
-        //     $bookid = 0;
-        // }
-        // if ($courseid == 0 || $bookid == 0) {
-        //     echo json_encode("Hey hey");
-        // } else {
-        //     echo json_encode($_POST);
-        // }
+        if ($_POST['courseid']=='non') {
+            $courseid = 0;
+        }
+        if ($_POST['bookid']=='non') {
+            $bookid = 0;
+        }
+        if ($courseid == 0 || $bookid == 0) {
+            echo json_encode("Hey hey");
+        } else {
+            echo json_encode($_POST);
+        }
     }
 }
