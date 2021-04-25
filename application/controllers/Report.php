24fab3692efa6e8874aa9d4f81f4a8c6d9596ca0
njovu <njovujsh@gmail.com>
2021-04-25 09:30:21 +0000

    first report add

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -717,7 +717,7 @@ class Report extends CI_Controller
             // );
             // array_push($array_mega, $sooth_array);
 
-            print_array($book_name_array);
+            print_array($book_id_array);
             print_array(".......................<br>");
             print_array($name_course_array);
             print_array("**********************<br>");
