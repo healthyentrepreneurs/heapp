2f3ce67ce98584393364bfff5cb496128d8ca5bf
njovu <njovujsh@gmail.com>
2021-05-25 11:31:44 +0000

    added values

--- application/controllers/Red.php
+++ application/controllers/Red.php
@@ -16,6 +16,13 @@ class Red extends CI_Controller
     }
     public function testdownload()
     {
-        echo 'Hello Joshua';
+        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+        $channel->queue_declare('hello', false, false, false, false);
+        $msg = new AMQPMessage('Hello World!');
+        $channel->basic_publish($msg, '', 'hello');
+        echo " [x] Sent 'Hello World!'\n";
+        $channel->close();
+        $connection->close();
     }
 }
