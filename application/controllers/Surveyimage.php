702249de0787de2f13aa21216ba10011f9480bb8
njovu <njovujsh@gmail.com>
2021-06-03 14:40:53 +0000

    added new survey logic

--- application/controllers/Surveyimage.php
+++ application/controllers/Surveyimage.php
@@ -32,7 +32,19 @@ class Surveyimage extends CI_Controller
         $this->load->library('upload', $config);
         if ($this->upload->do_upload()) {
             $data = array('upload_data' => $this->upload->data());
-            echo json_encode($data);
+            $name_file = $data['upload_data'];
+            // $_POST['image_name'] = $name_file['file_name'];
+            $array_image_survey = array(
+                'image_name' => $name_file['file_name'],
+                'user_id' => $this->input->post('user_id'),
+                'survey_id' => $this->input->post('survey_id')
+            );
+            $this->universal_model->updateOnDuplicate('survey_image', $array_image_survey);
+            $array_n = array(
+                'code' => "200",
+                'msg' => "Successfully Processed and Stored"
+            );
+            echo json_encode($array_n);
         } else {
             $error = array('error' => $this->upload->display_errors());
             echo json_encode($error);
