4d5e6932a4232f262ea43488ed949cc191f7c65b
njovu <njovujsh@gmail.com>
2021-08-25 22:58:18 +0000

    fix multiple emages

--- application/views/pages/admin/survey_instance.php
+++ application/views/pages/admin/survey_instance.php
@@ -28,6 +28,7 @@
                             <?php
                             $object_image=$value['text'];
                             if(is_array($object_image)){
+                                print_array($object_image);
 ?>
  <img src="<?= base_url('uploadsurvey/' . $object_image[1]['name']) ?>" alt="image" width="100" height="100" />
 <?php
