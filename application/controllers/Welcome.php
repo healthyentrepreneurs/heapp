06e4ca481a92e70f69f9ac9d1cd32d28fb304c1b
njovu <njovujsh@gmail.com>
2021-08-26 00:40:08 +0000

    fix multiple emages

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -445,6 +445,7 @@ class Welcome extends CI_Controller
 
                             //Old Support Version 1
                             $attempt_n_n_one = $this->universal_model->selectzy('imageifany', 'survey_report', 'id', $id, 'imageifany', "none");
+                            print_array($id);
                             if (!empty($attempt_n_n_one) && is_array($surveyobject[$keya])) {
                                 $jaja_image = array_shift($surveyobject[$keya]);
                                 if (!empty($jaja_image)) {
@@ -465,7 +466,7 @@ class Welcome extends CI_Controller
                                     $value_baby['value_name'] = "";
                                 }
                             } else {
-                                print_array($attempt_n_n_one);                                                          
+                                // print_array($attempt_n_n_one);                                                          
                                 // * Start New Image Versions
                                 // * To Be Back
                                 // $attempt_n_n_two=array();
