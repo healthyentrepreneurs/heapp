0959a587fbd9069741b2061f1b7e68fae86ddde6
njovu <njovujsh@gmail.com>
2021-08-27 16:33:18 +0000

    single  file

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -564,20 +564,18 @@ class Welcome extends CI_Controller
                                         echo "............";
                                         print_array($keya."  Njovu");
                                         echo "............";
-                                        print_array($surveyobject[$keya]);
+                                        print_array($surveyobject);
                                         echo "............";
                                         $attempt_n_n_one = $this->universal_model->selectz('imageifany', 'survey_report', 'id', $id);
                                         $array_one = array_shift($attempt_n_n_one);
                                         $is_none=$array_one['imageifany'];
-                                        if(array_key_exists('image-upload',$surveyobject[$keya]) && $is_none=="none"){
-                                            $arrayc['text'] =$surveyobject[$keya]['image-upload'];
-                                            $arrayc['value'] = $keya;
-                                        }else{
+                                        // if(array_key_exists('image-upload',$surveyobject[$keya]) && $is_none=="none"){
+                                        //     $arrayc['text'] =$surveyobject[$keya]['image-upload'];
+                                        //     $arrayc['value'] = $keya;
+                                        // }else{
                                             $arrayc['text'] =$is_none;
                                             $arrayc['value'] = $keya;
-                                        }
-                                       
-                                        // print_array("Here We are yy");
+                                        // }
                                     }
                                 }
                                 //End Tricky
