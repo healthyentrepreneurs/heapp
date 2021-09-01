188fff74128fdeb35c9f77f84a5367f6c4ee6ef1
njovu <njovujsh@gmail.com>
2021-09-01 15:24:07 +0000

    images zoome

--- application/views/pages/admin/survey_instance.php
+++ application/views/pages/admin/survey_instance.php
@@ -1,42 +1,6 @@
 <?php
 // print_array($survey_instance);
 ?>
-<style type="text/css">
-    #img-zoomer-box {
-  width: 500px;
-  height: auto;
-  position: relative;
-  margin-top: 10px;
-}
-
-#img-1 {
-  width: 100%;
-  height: auto;
-}
-
-#img-zoomer-box:hover, #img-zoomer-box:active {
-  cursor: zoom-in;
-  display: block;
-}
-
-#img-zoomer-box:hover #img-2, #img-zoomer-box:active #img-2 {
-  opacity: 1;
-}
-#img-2 {
-  width: 340px;
-  height: 340px;
-  background: url('https://bit.ly/2mgDw0y') no-repeat #FFF;
-  box-shadow: 0 5px 10px -2px rgba(0,0,0,0.3);
-  pointer-events: none;
-  position: absolute;
-  opacity: 0;
-  border: 4px solid whitesmoke;
-  z-index: 99;
-  border-radius: 100%;
-  display: block;
-  transition: opacity .2s;
-}
-</style>
 <div class="no-margin partition-white">
     <table class="table table-striped table-hover" id="instance_survey">
         <thead>
@@ -61,6 +25,7 @@
                     } else if (array_key_exists('text', $value) && $value['type'] == "file") {
                         // To be Revisted
                         $image_sci=$value['text'];
+                        // print_array($image_sci);
                         if(is_array($image_sci)){
                             $image_sci_second=array_shift($image_sci);
                             if(array_key_exists('name',$image_sci_second)){
@@ -69,11 +34,7 @@
                         }
                     ?>
                         <td>
-                        <div id="img-zoomer-box">
-  <img src="<?= base_url('uploadsurvey/' .$image_sci ) ?>" alt="image" id="img-1" />
-  <div id="img-2"></div>
-</div>
-                            <!-- <img src="<?= base_url('uploadsurvey/' .$image_sci ) ?>" alt="image" width="100" height="100" /> -->
+                            <img src="<?= base_url('uploadsurvey/' .$image_sci ) ?>" alt="image" width="100%" height="auto" />
                         </td>
                     <?php
                     } else {
