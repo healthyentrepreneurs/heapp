be356cc573a23ac49a7596ab3c703860cef0789f
njovu <njovujsh@gmail.com>
2021-09-01 12:59:02 +0000

    images survey

--- application/views/pages/admin/survey_instance.php
+++ application/views/pages/admin/survey_instance.php
@@ -25,7 +25,7 @@
                     } else if (array_key_exists('text', $value) && $value['type'] == "file") {
                         // To be Revisted
                         $image_sci=$value['text'];
-                        print_array($image_sci);
+                        // print_array($image_sci);
                         if(is_array($image_sci)){
                             $image_sci_second=array_shift($image_sci);
                             if(array_key_exists('name',$image_sci_second)){
