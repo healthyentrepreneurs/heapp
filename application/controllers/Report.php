b160626eefb78a3af62e56bccb80f63b50b686ea
joash <njovujsh@gmail.com>
2021-10-13 15:00:27 +0300

    added test

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -47,8 +47,8 @@ class Report extends CI_Controller
         $startdate = $this->input->post('startdate');
         $enddate = $this->input->post('enddate');
         $persial_survey = $this->universal_model->join_suv_reportpapa($surveyid, $startdate, $enddate);
-        $final_array = $this->report_surveydetails_data($persial_survey);
-        print_array($final_array);
+        // $final_array = $this->report_surveydetails_data($persial_survey);
+        print_array($persial_survey);
         //    echo json_encode($persial_survey);
     }
     public function report_surveydetails()
