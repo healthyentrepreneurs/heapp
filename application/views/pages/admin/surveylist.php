4e80fda3b79f283a3d63da62dc0f65fd2bec3112
njovu <njovujsh@gmail.com>
2021-12-04 08:49:18 +0000

    no more editing surveys

--- application/views/pages/admin/surveylist.php
+++ application/views/pages/admin/surveylist.php
@@ -47,7 +47,7 @@
                             <td class="center"><?= $value_in_sub['datecreated'] ?></td>
                             <td class="center">
                                 <div class="visible-md visible-lg hidden-sm hidden-xs">
-                                    <a href="<?= base_url('welcome/admin/3?id=' . $value_in_sub['id']) ?>" class="btn btn-xs btn-green tooltips" data-placement="top" data-original-title="View/Edit"><i class="fa fa-share"></i></a>
+                                    <!-- <a href="<?= base_url('welcome/admin/3?id=' . $value_in_sub['id']) ?>" class="btn btn-xs btn-green tooltips" data-placement="top" data-original-title="View/Edit"><i class="fa fa-share"></i></a> -->
                                     <a href="<?= base_url('welcome/admin/5?id=' . $value_in_sub['id']) ?>" class="btn btn-xs btn-green tooltips" data-placement="top" data-original-title="Update Image"><i class="fa fa-pencil fa-fw"></i></a>
                                     <a href="#" onclick="heyhey(<?= $value_in_sub['id'] ?>);" class="btn btn-xs btn-green tooltips" data-placement="top" data-original-title="Delete"><i class="fa fa-minus"></i></a>
                                 </div>
@@ -58,9 +58,9 @@
                                         </a>
                                         <ul role="menu" class="dropdown-menu pull-right dropdown-dark">
                                             <li>
-                                                <a role="menuitem" tabindex="-1" href="<?= base_url('welcome/admin/3?id=' . $value_in_sub['id']) ?>">
+                                                <!-- <a role="menuitem" tabindex="-1" href="<?= base_url('welcome/admin/3?id=' . $value_in_sub['id']) ?>">
                                                     <i class="fa fa-share"></i> View/Edit
-                                                </a>
+                                                </a> -->
                                             </li>
                                             <li>
                                                 <a role="menuitem" tabindex="-1" href="<?= base_url('welcome/admin/5?id=' . $value_in_sub['id']) ?>">
