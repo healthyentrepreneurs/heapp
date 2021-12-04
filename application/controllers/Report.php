0b5337286e535a5c8664199d0474c7ecdf7019f8
njovu <njovujsh@gmail.com>
2021-12-04 11:29:41 +0000

    add now

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -355,7 +355,7 @@ class Report extends CI_Controller
                             if (array_key_exists('title', $valuec)) {
                                 $arrayc['title'] = $valuec['title'];
                             }else {
-                                //Njovu changes to be 
+                                //Njovu when title is missing
                                 $arrayc['title'] = $valuec['name'];
                             }
                             if (array_key_exists('description', $valuec)) {
